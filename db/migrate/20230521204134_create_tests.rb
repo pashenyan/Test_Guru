093b46965ec13fc6309c91406051b61cd65161cc
Pavel <pavel@Noutbuk-Pavel.local>
2023-05-23 01:28:55 +0400

    a

--- db/migrate/20230521204134_create_tests.rb
+++ db/migrate/20230521204134_create_tests.rb
@@ -1,14 +1,14 @@
 class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
-      t.string :title, null: false
-      t.text :description
-      t.integer :duration, null: false
-      t.integer :price, null: false
+      t.string, null: false
+      t.text 
+      t.integer, null: false
+      t.integer, null: false
 
       t.timestamps
     end
-    
+
     def change
       change_column_default :tests, :level, from: nil, to: 1
     end  
