da22addf1c63822c46089e51365db1c2ad8916b4
Pavel <pavel@Noutbuk-Pavel.local>
2023-05-23 02:53:14 +0400

    corrected

--- db/migrate/20230522214220_create_categories.rb
+++ db/migrate/20230522214220_create_categories.rb
@@ -2,9 +2,6 @@ class CreateCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :categories do |t|
       t.string :title, null: false
-      t.text :description
-      t.integer :duration, null: false
-      t.integer :price, null: false
 
       t.timestamps
     end
