6a2533160f192ad3269b8c18670b4e0c6274966d
Pavel <pavel@Noutbuk-Pavel.local>
2023-05-24 02:45:25 +0400

    corrected

--- db/migrate/20230522215535_create_tests.rb
+++ db/migrate/20230522215535_create_tests.rb
@@ -2,7 +2,6 @@ class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
       t.string :title, null: false
-      t.string :testauthor, null: false
       t.integer :level, null: false, default: 1
       t.references :category, null: false, foreign_key: true
 
